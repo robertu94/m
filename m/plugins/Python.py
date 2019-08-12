1bc0be021621306f2d790db3af6595ceefd65bcd
Robert Underwood <rr.underwood94@gmail.com>
2019-08-12 16:46:54 -0500

    Add a python module

---
+++ m/plugins/Python.py
@@ -0,0 +1,40 @@
+from multiprocessing import cpu_count
+from subprocess import run
+from .Base import plugin, BasePlugin, PluginSupport
+
+@plugin
+class PythonSetupToolsPlugin(BasePlugin):
+
+    def build(self, settings):
+        """compiles the source code or a subset thereof"""
+        run(["python", "setup.py", "build"], cwd=settings['repo_base'].value)
+
+    def test(self, settings):
+        """runs automated tests on source code or a subset there of"""
+        run(["python", "setup.py", "test"], cwd=settings['repo_base'].value)
+
+    def clean(self, settings):
+        """cleans source code or a subset there of"""
+        run(["python", "setup.py", "clean"], cwd=settings['repo_base'].value)
+
+    def install(self, settings):
+        """cleans source code or a subset there of"""
+        run(["python", "setup.py", "install"], cwd=settings['repo_base'].value)
+
+
+    @staticmethod
+    def _supported(settings):
+        """returns a dictionary of supported functions"""
+        if 'repo_base' in  settings and (settings['repo_base'].value / "setup.py").exists():
+            state = PluginSupport.DEFAULT_MAIN
+        else:
+            state = PluginSupport.NOT_ENABLED_BY_REPOSITORY
+            
+        return {
+            "build": state,
+            "test": state,
+            "clean": state,
+            "install": state
+        }
+
+
--- m/plugins/__init__.py
+++ m/plugins/__init__.py
@@ -2,4 +2,5 @@ from . import Base
 from . import CMake
 from . import Autotools
 from . import Meson
+from . import Python
 from . import Settings
